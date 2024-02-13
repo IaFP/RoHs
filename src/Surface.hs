bd9a64674604f150bdc2422d63669e18aaa79b09
Apoorv Ingle <apoorv-ingle@uiowa.edu>
2024-02-13 10:10:47 -0600

    give primitives their own module

--- src/Surface.hs
+++ src/Surface.hs
@@ -3,11 +3,13 @@
 {-# LANGUAGE ImpredicativeTypes #-}  -- but was this applied before?  Otherwise, I'm not sure why my definitions ever typed...
 {-# LANGUAGE TypeFamilyDependencies #-}
 -- {-# OPTIONS -fforce-recomp -ddump-tc-trace -dcore-lint -fprint-explicit-kinds -fplugin RoHsPlugin #-}
-{-# OPTIONS -fforce-recomp -ddump-rn-trace -ddump-tc-trace -dcore-lint -fplugin RoHsPlugin #-}
+-- {-# OPTIONS -fforce-recomp -ddump-tc-trace -dcore-lint -fplugin RoHsPlugin #-}
+{-# OPTIONS -fforce-recomp -dcore-lint -fplugin RoHsPlugin #-}
 
 module Surface where
 
 import Common
+import RoHsPrimitives
 
 import Data.Proxy
 
@@ -25,86 +27,10 @@ foo1 r s = r `cat0` s
 
 -- (2) foo :: R0 (R '["x" := Int] ~+~ (R '["y" := Bool])) -- src plugin
 
--- (3)
--- foo :: R0 (R '["x" := Int] ~+~ (R '["y" := Bool]))
+-- (3) foo :: R0 (R '["x" := Int] ~+~ (R '["y" := Bool]))
 -- tc-plugin checks for this Plus (R '["x" := Int]) R ('["y" := Bool]) (R '["x" := Int] ~+~ (R '["y" := Bool]))
--- For now user cannot write Plus x y z. \tau type signatures.
--- foo_works :: forall z. Plus (R '["x" := Int]) ((R '["y" := Bool])) z => R0 z
--- foo_works =  (labR0 @"x" (1::Int)) `cat0` (labR0 @"y" (False::Bool))
-
--- Well this is potentially annoying...
-
-labR0 :: forall s {t}. t -> R0 (R '[s := t])
-labR0 = undefined
-
-unlabR0 :: R0 (R '[s := t]) -> t
-unlabR0 = undefined
-
-prj0 :: forall z y. z ~<~ y => R0 y -> R0 z
-prj0 = undefined
-
-cat0 :: R0 y -> R0 z -> R0 (y ~+~ z)
--- cat0 :: Plus y z x => R0 y -> R0 z -> R0 x
-cat0 _ _ = undefined
-
--- Sigh...
-
-labR1 :: forall s {f} {t}. f t -> R1 (R '[s := f]) t
-labR1 = undefined
-
-unlabR1 :: R1 (R '[s := f]) t -> f t
-unlabR1 = undefined
-
-prj1 :: z ~<~ y => R1 y t -> R1 z t
-prj1 = undefined
-
-cat1 :: R1 y t -> R1 z t -> R1 (y ~+~ z) t
--- cat1 :: Plus y z x => R1 y t -> R1 z t -> R1 x t
-cat1 _ _ = undefined
-
-sel0 :: forall s {t} {z}. R '[s := t] ~<~ z => R0 z -> t
--- Perhaps we can use some of these at least...
-sel0 r = unlabR0 @s (prj0 r)
-
-labV0 :: forall s {t}. t -> V0 (R '[s := t])
-labV0 = undefined
-
-labV1 :: forall s {f} {t}. f t -> V1 (R '[s := f]) t
-labV1 = undefined
-
-unlabV0 :: V0 (R '[s := t]) -> t
-unlabV0 = undefined
-
-unlabV1 :: V1 (R '[s := f]) t -> f t
-unlabV1 = undefined
-
-inj0 :: y ~<~ z => V0 y -> V0 z
-inj0 = undefined
-
-inj1 :: y ~<~ z => V1 y t -> V1 z t
-inj1 = undefined
-
-brn0 :: (V0 x -> t) -> (V0 y -> t) -> V0 (x ~+~ y) -> t
--- brn0 :: Plus x y z => (V0 x -> t) -> (V0 y -> t) -> V0 z -> t
-brn0 = undefined
-
-brn1 :: (V1 x t -> u) -> (V1 y t -> u) -> V1 (x ~+~ y) t -> u
--- brn1 :: Plus x y z => (V1 x t -> u) -> (V1 y t -> u) -> V1 z t -> u
-brn1 = undefined
-
--- and we can define
-
-con0 :: forall s {t} {z}. R '[s := t] ~<~ z => t -> V0 z
-con0 x = inj0 (labV0 @s x)
-
-case0 :: forall s {t} {u}. (t -> u) -> V0 (R '[s := t]) -> u
-case0 f = f . unlabV0   -- I am surprised GHC can figure this out... and somewhat concerned about what it's actually figured out
-
-con1 :: forall s {f} {t} {z}. R '[s := f] ~<~ z => f t -> V1 z t
-con1 x = inj1 (labV1 @s x)
-
-case1 :: forall s {f} {t} {u}. (f t -> u) -> V1 (R '[s := f]) t -> u
-case1 f = f . unlabV1
+foo_works :: Plus (R '["x" := Int]) ((R '["y" := Bool])) z => R0 z
+foo_works =  (labR0 @"x" (1::Int)) `cat0` (labR0 @"y" (False::Bool))
 
 --
 
@@ -127,54 +53,21 @@ bar1 = case0 @"true" id `brn0` case0 @"false" (\b -> if b then 0 else 1)
 
 --        inj0 @z
 
-bar2 :: forall y. V0 (R '["x" := Integer] ~+~ y) -> V0 (R '["x" := Bool] ~+~ y)
+bar2 :: forall z y1 y2.
+        -- Bit of a run-around here because GHC doesn't like `z ~<~ x ~+~ y` constraints
+        (Plus (R '["x" := Integer]) z y1,    -- `Integer` so defaulting doesn't get in the way
+         Plus (R '["x" := Bool]) z y2) =>
+        V0 y1 -> V0 y2
 bar2 = case0 @"x" (\i -> con0 @"x" (i == zero)) `brn0` inj0
   where zero :: Integer = 0
 
 
-ana0 :: -- forall z t.
-        -- (forall s y {u}. (Plus (R '[s := u]) y z) => u -> t) ->
-        -- V0 z -> t
-        forall t {s} {u} {y}.
-        (u -> t) ->
-        V0 ((R '[s := u]) ~+~ y) -> t
-ana0 _ = undefined
-
-anaE0 :: -- forall phi {z} {t}.
-         -- (forall s y {u}. (Plus (R '[s := u]) y z) => phi u -> t) ->
-         -- V0 (Each phi z) -> t
-         forall phi {s} {y} {u} {t}.
-         (phi u -> t) ->
-         V0 (Each phi ((R '[s := u]) ~+~ y)) -> t
-anaE0 _ = undefined
-
-anaA0 :: -- forall c {z} {t}.
-      --    All c z =>
-      --    (forall s y {u}. (Plus (R '[s := u]) y z, c u) =>
-      --                      Proxy s -> u -> t)  -- Assuming I'll need proxies for same reason as below
-      -- -> V0 z -> t
-         forall c {z} {t}.
-         (All c z)
-      => (forall s {u} {y}. (c u, ((R '[s := u]) ~+~ y ~ z)) => Proxy s -> u -> t)  -- Assuming I'll need proxies for same reason as below
-      -> V0 z -> t
-anaA0 _ = undefined
-
-
-showV :: forall {s} {u} {y}. (All Show (R '[ s := u ] ~+~ y)) => V0 (R '[ s := u ] ~+~ y) -> String
+showV :: forall z. All Show z => V0 z -> String
 showV = anaA0 @Show (const show)
 
-
-anaA1 :: forall c {z} {t} {u}.
-         (All c z)
-      => (forall s y {f}. (R '[ s := f ] ~+~ y ~ z, c f)
-                       => Proxy s -> f u -> t) -- Proxy!? see `fmapV` below
-      -> V1 z u -> t
-anaA1 _ = undefined
-
-
-showV1 :: All Show (R '[ s := u ] ~+~ y) => V0 (R '[ s := u ] ~+~ y) -> String
-showV1 = anaA0 @Show f -- ANI: should this be V1? No. Its version 1, not varient 1 (:
-  where f _ x = show x
+showV1 :: forall z. All Show z => V0 z -> String
+showV1 = anaA0 @Show f where
+  f _ x = show x
 
 -- But apparently adding the type signature will make `f` no longer have the
 -- right type.  I am concerned that I am missing something fundamental here...
@@ -193,17 +86,13 @@ showV1 = anaA0 @Show f -- ANI: should this be V1? No. Its version 1, not varient
 --   f x = con0 (\y -> x)
 
 eqV :: forall z. All Eq z => V0 z -> V0 z -> Bool
--- eqV :: forall s u y.
---        (Eq u, All Eq z)
---     => V0 (R '[ s := u ] ~+~ y) -> V0 (R '[ s := u ] ~+~ y) -> Bool
 eqV v w = anaA0 @Eq g w where
-  g :: forall (s :: Label) y u. (Eq u, ((R '[ s := u ]) ~+~ y ~ z)) =>  Proxy s -> u -> Bool
+  g :: forall s y t. (Plus (R '[s := t]) y z, Eq t)
+                  =>  Proxy s -> t -> Bool
   g _ x = (case0 @s (\y -> x == y) `brn0` const False) v
 
 
-fmapV :: forall a b z.
-         (All Functor z)
-      => (a -> b) -> V1 z a  -> V1 z b
+fmapV :: forall a b z. All Functor z => (a -> b) -> V1 z a -> V1 z b
 fmapV f = anaA1 @Functor g where
 
   -- Without the `Proxy s` argument, `g` types fine but doesn't play well as n
@@ -211,7 +100,7 @@ fmapV f = anaA1 @Functor g where
 
   -- Can't get away without the type annotation here... even if I try to pattern
   -- match on the proxy.  Let's pretend I understand anything.
-  g :: forall s y f. (Functor f, R '[ s := f] ~+~ y ~ z)
+  g :: forall s y f. (Plus (R '[s := f]) y z, Functor f)
                   => Proxy s -> f a -> V1 z b
   g _ x = con1 @s (fmap f x)
 
@@ -225,24 +114,17 @@ data TwoF a    = C2 a a
 
 newtype Mu f = Wrap {unwrap :: f (Mu f)}
 
-
+type BigR = R '["Const" := ZeroF Int] ~+~  R '["Add" := TwoF] ~+~ R '["Double" := OneF]
 type SmallR = R '["Const" := ZeroF Int] ~+~ R '["Add" := TwoF]
-type BigR = R '["Double" := OneF] ~+~ SmallR
 
 
-desugar' :: -- forall bigr smallr.
-            -- (-- These are essentially part of the type
-            --  Plus (R '["Double" := OneF]) smallr bigr,
-            --  All Functor smallr,
-            --  R '["Add" := TwoF] ~<~ smallr
-            -- ) =>
-            -- Mu (V1 bigr) -> Mu (V1 smallr)
-            forall smallr bigr.
-            (All Functor smallr
-            , R '["Double" := OneF] ~+~ smallr ~ bigr
-            , R '["Add" := TwoF] ~<~ smallr)
-         => Mu (V1 bigr) -> Mu (V1 smallr)
-
+desugar' :: forall bigr smallr.
+           (-- These are essentially part of the type
+            Plus (R '["Double" := OneF]) smallr bigr,
+            All Functor smallr,
+            R '["Add" := TwoF] ~<~ smallr
+           ) =>
+           Mu (V1 bigr) -> Mu (V1 smallr)
 desugar' (Wrap e) = Wrap ((double `brn1` (fmapV desugar' . inj1)) e) where
   double = case1 @"Double" (\(C1 x) -> con1 @"Add" (C2 (desugar' x) (desugar' x)))
 
