cd2cafdf94d16f854daefcf6a68fb005653e00a5
Apoorv Ingle <apoorv-ingle@uiowa.edu>
2024-06-24 14:05:43 -0500

    - split RecVariants and RecVariants2 - Add Untyped Lambda calculus example

--- src/RoHs/Examples/RecVariants2.hs
+++ src/RoHs/Examples/RecVariants2.hs
@@ -2,15 +2,14 @@
 {-# LANGUAGE FunctionalDependencies #-}  -- because TypeFamilyDependencies doesn't really do what I'd like yet...
 {-# LANGUAGE ImpredicativeTypes #-}  -- but was this applied before?  Otherwise, I'm not sure why my definitions ever typed...
 {-# LANGUAGE TypeFamilyDependencies #-}
-{-# LANGUAGE UnicodeSyntax #-}
 -- {-# OPTIONS -fforce-recomp -ddump-tc-trace -ddump-rn-trace -dcore-lint -fprint-explicit-kinds -fplugin RoHs.Plugin #-}
 -- {-# OPTIONS -fforce-recomp -ddump-tc-trace -dcore-lint -fplugin RoHs.Plugin #-}
 -- {-# OPTIONS -ddump-tc-trace -fforce-recomp -dcore-lint -ddump-ds -O0 -dasm-lint -dcmm-lint -ddump-asm-native -ddump-exitify -fplugin RoHs.Plugin -fplugin-opt debug #-}
-{-# OPTIONS -dcore-lint -O0 -fplugin RoHs.Plugin #-}
+-- {-# OPTIONS -dcore-lint -O0 -fplugin RoHs.Plugin #-}
 -- {-# OPTIONS -ddump-rn-trace -dcore-lint -O0 -ddump-tc-trace -ddump-simpl -ddump-ds-preopt -dverbose-core2core -dsuppress-module-prefixes -dno-suppress-type-applications -fplugin RoHs.Plugin #-}
 
 
-module RoHs.Examples.RecVariants where
+module RoHs.Examples.RecVariants2 where
 
 import RoHs.Language.Lib
 import RoHs.Examples.Variants
@@ -18,24 +17,22 @@ import RoHs.Examples.Variants
 
 default (Int)
 
--- This works for one level of unwrapping a knot tied extensiblke variant
+-- This is a genralized version of t ~> u to enable arbitrary nested case match
+-- The hope is to be able to peek more than once
 
 data t ~> u where
-  Rec :: (forall y. z ~<~ y => V1 z (Mu (V1 y)) -> (Mu (V1 y) -> u) -> u)
+  Rec :: (forall x y. Plus z x y => V1 z (Mu (V1 y)) -> (Mu (V1 y) -> u) -> u)
       -> Mu (V1 z) ~> u
-
 
 brnr :: forall v w {vw} {u}. Plus v w vw => ((Mu (V1 v)) ~> u) -> ((Mu (V1 w)) ~> u) -> ((Mu (V1 vw)) ~> u)
 brnr (Rec rfv) (Rec rfw) = Rec rfvw
   where
-  rfvw :: forall y. vw ~<~ y => (V1 vw (Mu (V1 y))) -> (Mu (V1 y) -> u) -> u
+  rfvw :: forall z y. Plus vw z y => (V1 vw (Mu (V1 y))) -> (Mu (V1 y) -> u) -> u
   rfvw = brn1 @v @w rfv rfw
 
-(~$~) :: (Mu (V1 x) ~> t) -> Mu (V1 x) -> t
-(Rec f) ~$~ (Mk x) = f x (Rec f ~$~)
+-- (~$~) :: (Mu (V1 x) ~> t) -> Mu (V1 x) -> t
+-- (Rec f) ~$~ (Mk x) = f x (Rec f ~$~)
 
--- Example 1.
--- Combining Zero, Suc, with Zero, Pred
 
 type NatPosR = R '[ "zeroP" := Zero (), "inc1" := One]
 type NatNegR = R '[ "zeroN" := Zero (), "dec1" := One]
@@ -74,35 +71,8 @@ evalN :: NatNegR ~<~ y => V1 NatNegR (Mu (V1 y)) -> (Mu (V1 y) -> Int) -> Int
 evalP = evalZP `brn1` evalIP
 evalN = evalZN `brn1` evalIN
 
-evalPN :: Mu (V1 (NatPosR ~+~ NatNegR)) -> Int
-evalPN = (~$~) ((Rec evalP) `brnr` (Rec evalN))
+-- evalPN = (~$~) ((Rec evalP) `brnr` (Rec evalN))
 
 
--- Example 2.
--- Untyped Lambda Calculus
-
-type LamR = R '[ "var" := Zero Int, "lam" := One, "app" := Two]
-
-
-mkVar :: R '["var" := Zero Int] ~<~ z => Int -> Mu (V1 z)
-mkVar i = Mk (con1 @"var" (Z i))
-
-mkApp :: (R '["app" := Two] ~<~ z) => Mu (V1 z) -> Mu (V1 z) -> Mu (V1 z)
-mkApp e1 e2 = Mk (con1 @"app" (T e1 e2))
-
-mkLam :: (R '["lam" := One] ~<~ z) => Mu (V1 z) -> Mu (V1 z)
-mkLam e = Mk (con1 @"lam" (O e))
-
-showVar :: V1 (R '["var" := Zero Int]) t -> p -> String
-showVar e _ = case1 @"var" (\(Z i) -> show i) e
-
-showLam :: V1 (R '["lam" := One]) t -> (t -> String) -> String
-showLam e r = case1 @"lam" (\(O b) -> "(λ " ++ r b ++ ")") e
-
-showApp :: V1 (R '["app" := Two]) t -> (t -> String) -> String
-showApp e r = case1 @"app" (\(T fn a) -> (r fn)  ++ " " ++ (r a)) e
-
-showTerm x = ((Rec showVar) `brnr` (Rec showLam) `brnr` (Rec showApp)) ~$~ x
-
-idLam :: Mu (V1 LamR)
-idLam = mkApp (mkLam (mkVar 0)) (mkVar 0)
+-- halfP :: NatPosR ~<~ y => V1 NatPosR (Mu (V1 y)) -> (Mu (V1 y) -> Int) -> Int
+-- halfP
